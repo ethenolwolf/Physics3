6cf399840bf07e7908ad864d930ebd77753d35fe
Naved Ahmad <ethenolwolf@gmail.com>
2016-09-17 06:28:58 +0530

    Second Commit

---
+++ desktop/src/com/gfs/physics3/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gfs.physics3.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gfs.physics3.Physics3;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Physics3(), config);
+	}
+}
